09a4014b7f9c252a3968d437f25e7dae8792f5a1
fatihsenyigit <fsenyigit8@gmail.com>
2023-07-17 00:07:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import Sayac from "./Contract/Sayac.json"
 
 function App() {
+  const [address, setAddress] = useState("")
+  useEffect(() => {
+    (async () => {
+      const provider =  new ethers.BrowserProvider(window.ethereum, "any");
+      await provider.send ("eth_requestAccounts",[]);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setAddress(address);
+
+      const sayacContractAddress = "0x1a4cbaB1e2bb13b79f38b4098df5Df737Ad95e30";
+      
+        const contract =   new ethers.Contract(
+          sayacContractAddress,
+          Sayac.abi,
+          provider
+        );
+        console.log(contract);
+        
+        const _number = await contract.number();
+        const _increment = await contract.increment()
+        console.log(_number);
+        console.log(_increment);
+
+      
+      
+      
+    })();
+  
+    return () => {
+      
+    }
+  }, [])
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Your address: {address}</h3>
     </div>
   );
 }
